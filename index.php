1bdadb1530b9f82991005b867bedf6e1813cf2bf
Jakub Brzegowski <symfiz@gmail.com>
2015-10-10 11:35:20 +0200

    Useless comment deleted.

--- index.php
+++ index.php
@@ -8,15 +8,3 @@ require_once BASEPATH.'/Bootstrap.php';
 
 
 $bootstrap = new Bootstrap();
-
-//echo $_GET['path'];
-//
-//echo BASEPATH;
-//br();
-//echo 'host'.$_SERVER['HTTP_HOST'];
-//br();
-//echo 'self' . $_SERVER['PHP_SELF'];
-//br();
-//echo 'request_uri'.filter_input(INPUT_SERVER, $_SERVER['REQUEST_URI']);
-
-
