9f2bf43fc7df98e7562658b007003b210a3bc25f
Jakub Brzegowski <symfiz@gmail.com>
2015-10-13 13:57:24 +0200

    Started writing head.php and foot.php

--- application/views/head.php
+++ application/views/head.php
@@ -1,8 +1,9 @@
-<?php
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+ <title>My page title</title>
+</head>
+<body>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
